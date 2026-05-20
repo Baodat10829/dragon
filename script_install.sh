5ee5298e789e26f3f0efd4196226fa51df66592f
Baodat10829 <132512707+Baodat10829@users.noreply.github.com>
2026-05-20 17:25:00 +0700

    Delete script_install.sh

--- script_install.sh
+++
@@ -1,9 +0,0 @@
-yes | pkg update -y
-yes | pkg upgrade -y
-yes | pkg install openjdk-17 python p7zip -y
-yes | pip install pymysql
-
-curl -L -O https://raw.githubusercontent.com/Baodat10829/dragon/main/install.sh
-curl -L -O https://github.com/Baodat10829/dragon/releases/download/V983948/mod.7z
-chmod +x ~/../usr/bin/*.sh
-./install.sh
